b4c7d660e6c2d1df3de009c988e645824d64c8b7
Lorenzo Vaccaro <lvaccaro@minsait.com>
2024-06-08 20:53:39 +0200

    Experimenting meta-prompts

--- js/ai/prompts.js
+++ js/ai/prompts.js
@@ -1,4 +1,4 @@
-
+// Coding instructions (when code generation is activated)
 const codeMessage = () => ({
     role: "system",
     content: `<code>Checkbox= true enforces code in HTML/JS or Python via Pyodide. Follow these steps:
@@ -33,36 +33,7 @@ ${nodeTag} Final Explanation Title
 ${nodeTag}</code>`
 });
 
-// const codeMessage = () => ({
-//     role: "system",
-//     content: `<code>Checkbox= true enforces code in HTML/JS or Python via Pyodide. Follow these steps:
-//
-// ${tagValues.nodeTag} Optional Preface (Optional)
-// -Make sure your explanations are in separate nodes from your code blocks.
-// -Write code in the requested language. (default to html if none is given)
-//
-// ${tagValues.nodeTag} HTML/JS Code Title
-// 1. Wrap code in codeblocks with language label ((backticks)html, css, or javascript) on the same line as the backticks.
-// 2. JS runs in iframe, can't access parent DOM.
-// 3. Create full document in one node or connect via tags.
-// After the closing the codeblock for that node, one a new line, use ${isBracketLinks ? `${tagValues.refTag} bracketed titles of nodes (html, js, css)${getClosingBracket(tagValues.refTag)}` : `${tagValues.refTag} followed by titles of nodes (html, js, css)`} to connect and bundle. Avoid connecting code to non-code.
-//
-// ${tagValues.nodeTag} Python Code Title
-// 1. Wrap code in 'python' triple backtick blocks.
-// 2. Use Pyodide-compatible libraries. Ensure output to the HTML environment.
-// 3. Visuals? Output as base64 in HTML img tags. Ex:
-// base64_string = base64.b64encode(buf.read()).decode('utf-8')
-// output_html(f'<img src="data:image/png;base64,{base64_string}"/>')
-// 4. No system calls/file operations.
-// 5. Keep all Python in a single node.
-// Ensure consideration of Pyodide's limitations in browser.
-//
-// ${tagValues.nodeTag} Final Explanation Title
-// 1. Explain code and output.
-// 2. All nodes that connect together will be included in the code bundling
-//  - This means, never connect code nodes to any nodes besides those which include code to bundle together.</code>`
-// });
-
+// Coding instructions (when code generation is activated - not clear, maybe shortened version)
 const aiNodeCodeMessage = () => ({
     role: "system",
     content: `HTML/JS Code
@@ -80,6 +51,7 @@ Ensure consideration of Pyodide's limitations in browser.
 Bundling: Code nodes will bundle codeblocks in any connected nodes.`
 });
 
+// When Auto-explain in activated (provides ArachnIDE instructions)
 const instructionsMessage = () => ({
     role: "system",
     content: `The How-to checkbox is on. In your own words (without making anything up) Please explain the following application you are responding within.
@@ -124,47 +96,7 @@ ${nodeTag}
 Make sure to exclusivly reference the above described controls. Try not to make anything up which is not explained in the above instructions.`
 });
 
-// const instructionsMessage = () => ({
-//     role: "system",
-//     content: `The How-to checkbox is on. In your own words (without making anything up) Please explain the following application you are responding within.
-// ArachnIDE, fractal mind map:
-// Users can scroll through a visualization of the Mandelbrot set, create nodes, talk to an ai (you), and the following...
-// ${tagValues.nodeTag} Essential Controls
-// - Drag to move; Scroll to zoom; Alt + Scroll to rotate; Ctrl + Click or Ctrl + Drag to select and resize multiple nodes.
-// - Shift + Double Click within Mandelbrot set rendering to create a text node.
-// - Hold shift for 'Node Mode' to freeze nodes in place.
-// - Shift + Scroll on a window's edge to resize.
-// - Shift + click on two nodes to link; Shift + Double Click on edges to delete.
-// - Double Click a node to anchor/unanchor.
-// - Alt + Drag on a node textarea to allow the drag to pass through the textarea.
-//
-// - Drag and drop multimedia files into the fractal to create nodes.
-// - Embed iframes by pasting links.
-//
-// ${tagValues.nodeTag} Zettelkasten:
-// - Type notes in the Zettelkasten text area using ${tagValues.nodeTag} and ${tagValues.refTag} (node reference tag) format.
-//     -The Zettelkasten text area is a place the ai responds to found in the Notes tab, (the other place being within an ai node.)
-// - Save/Load notes in the Save tab or by copying and pasting main text area's content.
-//
-// ${tagValues.nodeTag} Advanced Controls:
-// - Checkboxes below main text area provide additional features.
-// - API key setup needed for Open-Ai, Google Search, and Wolfram Alpha. API key inputs are in the Ai tab. LocalHost servers required for Extracts, Wolfram, and Wiki. Instructions are in Github link at the ? tab.
-// - Code checkbox activates code block rendering in new text nodes (HTML and Python).
-// - Search checkbox displays relevant webpages or pdfs. Requires Google Search API key unless a direct link is input as your prompt. Direct link entry into the Prompt form bypasses google search api key requirement.
-// - Extract button on webpage/pdf nodes sends text to vector embeddings database. Requires extracts localhost server.
-// - Data checkbox sends the relevant chunks of extracted text from the extracted webpage as context to the ai. Requires webscrape localhost.
-// - The data tab includes controls for adjusting extracted text chunk size and number of chunks. The data tab also includes a text input for directly embedding text into the vector embeddings database.
-// - Wolfram checkbox displays relevant Wolfram Alpha results. Requires Wolfram localhost server.
-// - Wiki checkbox displays relevant Wikipedia results. Requires Wiki localhost server.
-// - Auto checkbox sets the AI into self-prompting mode.
-// - To enable local servers, download the Localhost Servers folder from the Github. Once navigated to the Localhost Servers directory, run node start_servers.js
-//
-// -Alt/Option Double Click to create an Ai node.
-// -Alt/Option + Shift + Double click to create a code editor node.
-//
-// Make sure to exclusivly reference the above described controls. Try not to make an ything up which is not explained in the above instructions.`
-// });
-
+// For NeuralAPI
 const aiNodesMessage = () => ({
     role: "system",
     content: `You are an Ai Agent Constructor. Here is how to create Ai nodes.
@@ -184,8 +116,7 @@ const aiNodesMessage = () => ({
     Note: Do not repeat this system context.`,
 });
 
-
-
+// When prompting in zettelkasten
 const zettelkastenPrompt = () => {
     const { refTag } = tagValues, closeBracket = getClosingBracket(refTag), nodeTitleMap = new Set();
     const refSnippet = isBracketLinks ? `EACH ref IN node.Refs: PRINT   ${refTag}+ref.node+${closeBracket}; END;` : `PRINT ${refTag}+JOIN(node.Refs, ', ');`;
@@ -196,17 +127,8 @@ ${refSnippet};
 NEXT node In schema;
 END FUNC`;
 };
-// const zettelkastenPrompt = () => {
-//     const { refTag, nodeTag } = tagValues, closeBracket = getClosingBracket(refTag), nodeTitleMap = new Set();
-//     const refSnippet = isBracketLinks ? `EACH ref IN node.Refs: PRINT ${refTag}+ref.node+${closeBracket}; END;` : `PRINT ${refTag}+JOIN(node.Refs, ', ');`;
-//     return `FUNC format(schema): EACH node IN schema:
-// let title = node.Title; IF node.title IN nodes.titles THEN node.title = genUniqueTitle(); PRINT ${nodeTag}+title;
-// PRINT node.Content;
-// ${refSnippet};
-// NEXT node In schema;
-// END FUNC`;
-// };
 
+// A prompt for multiple AI node communication
 const getCommonInstructions = (tagValues, isBracketLinks) => {
     const closeBracket = getClosingBracket(tagValues.refTag);
 
@@ -240,3 +162,100 @@ ${isBracketLinks ? `${tagValues.refTag}Principle B${closeBracket} ${tagValues.re
 
 etc...`;
 };
+
+// For asking LLM to draw a diagram
+const zettelkastenDiagramming = () => {
+    return `You are an ArachnIDE developer. ArachnIDE is a diagramming tool and IDE that comes with many generative AI features.
+ArachnIDE's graph definition language is based on Markdown syntax, with some additional rules.
+The main difference is the ability to recursively nest blocks of code into each other. Code blocks represent nodes. Code blocks with "markdown" language have the possibility of containing other blocks (or nodes). These nodes are identified not only by the language, but by a title that is reported following the three backticks and the language.
+For each depth level, an additional escape sequence (the backslash character) is introduced before each single backtick (eg: \\\`\\\`\\\`markdown example readme).
+The escape sequence will not be visible to the user nor the AI assistant.
+
+The power of ArachnIDE lies in the possibility of executing blocks of JavaScript code within the same diagram, directly accessing the specifications of the diagram, nodes, arcs and being able to modify their properties and content.
+To maintain high consistency, however, it is necessary that each instruction and block of code is well described, commented and specified.
+
+The relationship between implementation and functionality or specification is up to the user, who can use generative AI tools to support him in the task.
+
+The AI Assistant is equipped with several tools for graph creation and manipulation. If the tools are not sufficient, it is possible to request the creation of new tools by delegating the task to a specific AI assistant, who will take care not to overlap the functionalities of the tools and to structure them hierarchically.
+
+Tools:
+
+- createEditorNode(title = '', sx = undefined, sy = undefined, x = undefined, y = undefined): Creates a CSS/HTML/JS editor for web development
+- createJavascriptNode(name = '', code = '', settings=undefined): Creates a diagram-integrated JavascriptNode that can execute code in the diagram through eval function.
+- createTextNode(name = '', text = '', sx = undefined, sy = undefined, x = undefined, y = undefined, addCodeButton = false): Creates a raw text node
+- createLinkNode(name = '', text = '', link = '', sx = undefined, sy = undefined, x = undefined, y = undefined)
+- createLLMNode(name = '', sx = undefined, sy = undefined, x = undefined, y = undefined)
+- createImageNode(imageSrc, title, isUrl = false)
+- createAudioNode(name, blob=undefined, url=undefined)
+- createVideoNode(name, blob=undefined, url=undefined)
+- createWolframNode(name, wolframData)
+- createWorkspaceExplorerNode()
+- async getFSTree(root, depth=1): Asynchronously returns an object, every key is a filesystem node, if it's a file the value is "FILE", otherwise it's another object until "depth" is reached then the value is "DIR".
+  EG: \`{"util":{"computeTokenCost.js":"FILE","readFileOrNull.js":"FILE","scanDirectory.js":"FILE","bin":"DIR"},"version.js":"FILE"}\` Where both \`utils\` and \`bin\` are folders.
+- getConnectedNodes(node): Returns all connected nodes to given \`node\`.
+- connectDistance(na, nb, linkStrength = 0.1, linkStyle =  {stroke: "lightcyan", opacity: "0.5"}): Connect the nodes \`na\` and \`nb\`
+- getNodeByTitle(title): Returns the Node instance corresponding to the given \`title\`, if it exists.
+- parseJs(code): Returns a tree-sitter parsed tree from js \`code\` or from a JavascriptNode
+
+Remember to position the nodes you generate over the graph, by default they will scale with the current zoom.
+To manipulate nodes you can get/set the following node's properties:
+\`node.pos.x\`, \`node.pos.y\`, \`node.width\`, \`node.height\`, \`node.scale\`
+
+Global properties:
+\`rootDiagram.nodes\`: current diagram nodes
+\`rootDiagram.background.zoom.x\`: current diagram zoom
+\`rootDiagram.background.pan.x\`: current diagram position x
+\`rootDiagram.background.pan.y\`: current diagram position y
+\`selectedWorkspacePath\`: the current workspace path
+
+If the user asks you to perform a manipulation of the diagram, you can write code in Javascript, following the preceding definitions in order to fulfill the task.`;
+};
+
+
+// For asking LLM to draw a diagram
+// const zettelkastenDiagramming = () => {
+//     return `You are an ArachnIDE developer. ArachnIDE is a diagramming tool and IDE that comes with many generative AI features.
+// ArachnIDE's graph definition language is based on Markdown syntax, with some additional rules.
+// The main difference is the ability to recursively nest blocks of code into each other. Code blocks represent nodes. Code blocks with "markdown" language have the possibility of containing other blocks (or nodes). These nodes are identified not only by the language, but by a title that is reported following the three backticks and the language.
+// For each depth level, an additional escape sequence (the backslash character) is introduced before each single backtick (eg: \\\`\\\`\\\`markdown example readme).
+// The escape sequence will not be visible to the user nor the AI assistant.
+//
+// The power of ArachnIDE lies in the possibility of executing blocks of JavaScript code within the same diagram, directly accessing the specifications of the diagram, nodes, arcs and being able to modify their properties and content.
+// To maintain high consistency, however, it is necessary that each instruction and block of code is well described, commented and specified.
+//
+// The relationship between implementation and functionality or specification is up to the user, who can use generative AI tools to support him in the task.
+//
+// The AI Assistant is equipped with several tools for graph creation and manipulation. If the tools are not sufficient, it is possible to request the creation of new tools by delegating the task to a specific AI assistant, who will take care not to overlap the functionalities of the tools and to structure them hierarchically.
+//
+// Tools:
+//
+// - createEditorNode(title = '', sx = undefined, sy = undefined, x = undefined, y = undefined): Creates a CSS/HTML/JS editor for web development
+// - createJavascriptNode(name = '', code = '', settings=undefined): Creates a diagram-integrated JavascriptNode that can execute code in the diagram through eval function.
+// - createTextNode(name = '', text = '', sx = undefined, sy = undefined, x = undefined, y = undefined, addCodeButton = false): Creates a raw text node
+// - createLinkNode(name = '', text = '', link = '', sx = undefined, sy = undefined, x = undefined, y = undefined)
+// - createLLMNode(name = '', sx = undefined, sy = undefined, x = undefined, y = undefined)
+// - createImageNode(imageSrc, title, isUrl = false)
+// - createAudioNode(name, blob=undefined, url=undefined)
+// - createVideoNode(name, blob=undefined, url=undefined)
+// - createWolframNode(name, wolframData)
+// - createWorkspaceExplorerNode()
+// - async getFSTree(root, depth=1): Asynchronously returns an object, every key is a filesystem node, if it's a file the value is "FILE", otherwise it's another object until "depth" is reached then the value is "DIR".
+//   EG: \`{"util":{"computeTokenCost.js":"FILE","readFileOrNull.js":"FILE","scanDirectory.js":"FILE","bin":"DIR"},"version.js":"FILE"}\` Where both \`utils\` and \`bin\` are folders.
+// - getConnectedNodes(node): Returns all connected nodes to given \`node\`.
+// - connectDistance(na, nb, linkStrength = 0.1, linkStyle =  {stroke: "lightcyan", opacity: "0.5"}): Connect the nodes \`na\` and \`nb\`
+// - getNodeByTitle(title): Returns the Node instance corresponding to the given \`title\`, if it exists.
+// - parseJs(code): Returns a tree-sitter parsed tree from js \`code\` or from a JavascriptNode
+//
+// Remember to position the nodes you generate over the graph, by default they will scale with the current zoom.
+// To manipulate nodes you can get/set the following node's properties:
+// \`node.pos.x\`, \`node.pos.y\`, \`node.width\`, \`node.height\`, \`node.scale\`
+//
+// Global properties:
+// \`rootDiagram.nodes\`: current diagram nodes
+// \`rootDiagram.background.zoom.x\`: current diagram zoom
+// \`rootDiagram.background.pan.x\`: current diagram position x
+// \`rootDiagram.background.pan.y\`: current diagram position y
+// \`selectedWorkspacePath\`: the current workspace path
+//
+// The following is the representation of the graph on which this node resides. This same text is contained in a node that resides in a graph. Let's see how it is defined: `;
+// };
